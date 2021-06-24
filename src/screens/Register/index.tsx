7aa858c83b72600c7b92a97719b3a686979d73ae
Bruno Paixao <bruno.paixao@conexasaude.com.br>
2021-06-24 20:16:12 -0300

    creating Input and Button components

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
+
+import { Container, Header, Title, Form, Fields } from './styles';
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input 
+            placeholder='Nome' 
+          />
+          <Input 
+            placeholder='Preco' 
+          />
+        </Fields>
+
+        <Button title='Enviar' />
+      </Form>
+
+    </Container>
+  )
+}
