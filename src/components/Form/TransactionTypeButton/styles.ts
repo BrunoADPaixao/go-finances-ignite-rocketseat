363a89f8276f9fc3b1d6dcb2f068d5fe42f72ff3
Bruno Paixao <bruno.paixao@conexasaude.com.br>
2021-07-03 12:30:20 -0300

    refactoring touchable buttons with react-native-gesture-hanlder components

--- src/components/Form/TransactionTypeButton/styles.ts
+++ src/components/Form/TransactionTypeButton/styles.ts
@@ -1,8 +1,7 @@
 import styled, { css } from "styled-components/native";
-import { TouchableOpacity } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
-import theme from "../../../global/styles/theme";
 
 interface ContainerProps {
   isActive: boolean;
@@ -13,17 +12,12 @@ interface IconProps {
   type: 'up' | 'down';
 }
 
-export const Container = styled(TouchableOpacity)<ContainerProps>`
+export const Container = styled.View<ContainerProps>`
   width: 48%;
-
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
 
   border: 1.5px solid ${({theme}) => theme.colors.text};
   border-radius: 5px;
 
-  padding: 16px;
 
   ${({ isActive, type }) => isActive && type === 'up' && css`
     background-color: ${({ theme }) => theme.colors.success_light};
@@ -34,6 +28,14 @@ export const Container = styled(TouchableOpacity)<ContainerProps>`
     background-color: ${({ theme }) => theme.colors.attention_light};
     border: none;
   `};
+`;
+
+export const Button = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding: 16px;
 `;
 
 export const Icon = styled(Feather)<IconProps>`
